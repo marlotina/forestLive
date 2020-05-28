3a94e6fc689182357001de40b292e63041cc6942
marlotina <inaki.arrieta@gmail.com>
2020-05-28 19:00:06 +0200

    loggin projects

---
+++ Framework/FL.LogTrace.Contracts.Standard/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FL.LogTrace.Contracts.Standard
+{
+    public interface ILogger<out T>
+    {
+        void LogDebug(string message);
+        void LogDebug(Exception exception, string message = "");
+
+        void LogInfo(string message);
+        void LogInfo(Exception exception, string message = "");
+
+        void LogWarning(string message);
+        void LogWarning(Exception exception, string message = "");
+
+        void LogError(string message);
+        void LogError(Exception exception, string message = "");
+    }
+}
---
+++ Framework/FL.Logging.Implementation.Standard/Logger.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace FL.Logging.Implementation.Standard
+{
+    public class Logger<T>: LogTrace.Contracts.Standard.ILogger<T>
+    {
+        ILogger<T> iLogger;
+
+        public Logger(ILogger<T> iLogger)
+        {
+            this.iLogger = iLogger;
+        }
+
+        public void LogDebug(string message)
+        {
+            this.iLogger.LogDebug(message);
+        }
+
+        public void LogDebug(Exception exception, string message = "")
+        {
+            this.iLogger.LogDebug(exception, message);
+        }
+
+        public void LogInfo(string message)
+        {
+            this.iLogger.LogInformation(message);
+        }
+
+        public void LogInfo(Exception exception, string message = "")
+        {
+            this.iLogger.LogInformation(exception, string.Empty);
+        }
+
+        public void LogWarning(string message)
+        {
+            this.iLogger.LogWarning(message);
+        }
+
+        public void LogWarning(Exception exception, string message = "")
+        {
+            this.iLogger.LogWarning(exception, string.Empty);
+        }
+
+        public void LogError(string message)
+        {
+            this.iLogger.LogError(message);
+        }
+
+        public void LogError(Exception exception, string message = "")
+        {
+            this.iLogger.LogError(exception, string.Empty);
+        }
+    }
+}
