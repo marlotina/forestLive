582f1454aa8a6cd28881883be8d0b61a4c984bd6
marlotina <inaki.arrieta@gmail.com>
2021-02-21 02:01:31 +0100

    new version robot

--- Tools/FL.Console.Core.RobotSpecies/FL.Console.Robot.Species/Program.cs
+++ Tools/FL.Console.Core.RobotSpecies/FL.Console.Robot.Species/Program.cs
@@ -32,131 +32,95 @@ namespace FL.Console.Robot.Species
 
         static void Main(string[] args)
         {
-            var comonBirdsHtml = GetTheHtml(URL_WIKI_BIRDS_BY_COMMON_NAME);
-            var birdsGropuByLetter = GetNodes(comonBirdsHtml, "//div[@class= 'div-col']");
-
-            foreach (HtmlNode node in birdsGropuByLetter)
+            try
             {
-                var links = GetNodes(node.InnerHtml, "//a");
+                var comonBirdsHtml = GetTheHtml(URL_WIKI_BIRDS_BY_COMMON_NAME);
+                var birdsGropuByLetter = GetNodes(comonBirdsHtml, "//div[@class= 'div-col']");
 
-                foreach (var link in links)
+                foreach (HtmlNode node in birdsGropuByLetter)
                 {
-                    var birdItem = new BirdSpecie();
-                    birdItem.SpecieId = Guid.NewGuid();
+                    var links = GetNodes(node.InnerHtml, "//a");
 
-                    var url_EN = DOMAIN_WIKI_EN + link.Attributes["href"].Value;
-                    var birdPageHtml = GetTheHtml(url_EN);
-
-                    birdItem.ScienceName = GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//i").FirstOrDefault().InnerText;
-                    var birdDataLanguage_EN = new BirdDataLanguage()
+                    foreach (var link in links)
                     {
-                        Url = url_EN,
-                        Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                        LanguageId = Guid.Parse(ID_LANGUAGE_EN)
-                    };
-                    birdItem.Data_EN = birdDataLanguage_EN;
+                        var birdItem = new BirdSpecie();
+                        birdItem.SpecieId = Guid.NewGuid();
 
-                    //spanish
-                    var nodeBirdLanguage = GetNodes(birdPageHtml, $"//li[@class= '{LANGUAGE_ES}']//a");
-                    if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
-                    {
-                        var url_ES = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
-                        birdPageHtml = GetTheHtml(url_ES);
+                        var url_EN = DOMAIN_WIKI_EN + link.Attributes["href"].Value;
+                        var birdPageHtml = GetTheHtml(url_EN);
 
-                        var birdDataLanguage_ES = new BirdDataLanguage()
-                        {
-                            Url = url_ES,
-                            Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                            LanguageId = Guid.Parse(ID_LANGUAGE_ES)
-                        };
-                        birdItem.Data_ES = birdDataLanguage_ES;
+                        var nodes = GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//i");
+
+                        if (nodes != null && nodes.Any()) {
+                            birdItem.ScienceName = nodes.FirstOrDefault().InnerText;
+                            var birdDataLanguage_EN = new BirdDataLanguage()
+                            {
+                                Url = url_EN,
+                                Name = HttpUtility.HtmlEncode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
+                                LanguageId = Guid.Parse(ID_LANGUAGE_EN)
+                            };
+                            birdItem.Data_EN = birdDataLanguage_EN;
+                        }
+                        
+
+                        //spanish
+                        birdItem.Data_ES = GetBirdLanguageData(birdPageHtml, LANGUAGE_ES, ID_LANGUAGE_ES);
+                        //portugues
+                        birdItem.Data_PT = GetBirdLanguageData(birdPageHtml, LANGUAGE_PT, ID_LANGUAGE_PT);
+                        //Deutch
+                        birdItem.Data_DE = GetBirdLanguageData(birdPageHtml, LANGUAGE_DE, ID_LANGUAGE_DE);
+                        //Italian
+                        birdItem.Data_IT = GetBirdLanguageData(birdPageHtml, LANGUAGE_IT, ID_LANGUAGE_IT);
+                        //French
+                        birdItem.Data_FR = GetBirdLanguageData_FR(birdPageHtml, LANGUAGE_FR, ID_LANGUAGE_FR);
+
+                        InstertInDatabse(birdItem);
                     }
-
-                    //portugues
-                    nodeBirdLanguage = GetNodes(birdPageHtml, $"//li[@class= '{LANGUAGE_PT}']//a");
-                    if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
-                    {
-                        var url_PT = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
-                        birdPageHtml = GetTheHtml(url_PT);
-
-                        var birdDataLanguage_PT = new BirdDataLanguage()
-                        {
-                            Url = url_PT,
-                            Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                            LanguageId = Guid.Parse(ID_LANGUAGE_PT)
-                        };
-                        birdItem.Data_PT = birdDataLanguage_PT;
-                    }
-
-                    //Deutch
-                    nodeBirdLanguage = GetNodes(birdPageHtml, $"//li[@class= '{LANGUAGE_DE}']//a");
-                    if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
-                    {
-                        var url_DE = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
-                        birdPageHtml = GetTheHtml(url_DE);
-
-                        var birdDataLanguage_DE = new BirdDataLanguage()
-                        {
-                            Url = url_DE,
-                            Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                            LanguageId = Guid.Parse(ID_LANGUAGE_DE)
-                        };
-                        birdItem.Data_DE = birdDataLanguage_DE;
-                    }
-
-                    //Italian
-                    nodeBirdLanguage = GetNodes(birdPageHtml, $"//li[@class= '{LANGUAGE_IT}']//a");
-                    if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
-                    {
-                        var url_IT = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
-                        birdPageHtml = GetTheHtml(url_IT);
-
-                        var birdDataLanguage_IT = new BirdDataLanguage()
-                        {
-                            Url = url_IT,
-                            Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                            LanguageId = Guid.Parse(ID_LANGUAGE_IT)
-                        };
-                        birdItem.Data_IT = birdDataLanguage_IT;
-                    }
-
-                    //French
-                    nodeBirdLanguage = GetNodes(birdPageHtml, $"//li[@class= '{LANGUAGE_FR}']//a");
-                    if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
-                    {
-                        var url_FR = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
-                        birdPageHtml = GetTheHtml(url_FR);
-
-                        var birdDataLanguage_FR = new BirdDataLanguage()
-                        {
-                            Url = url_FR,
-                            Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                            LanguageId = Guid.Parse(ID_LANGUAGE_FR)
-                        };
-                        birdItem.Data_FR = birdDataLanguage_FR;
-                    }
-
-                    InstertInDatabse(birdItem);
                 }
+            }
+            catch (Exception ex) 
+            { 
             }
         }
 
-        private static BirdDataLanguage GetLanguageData(string html, string languageClassHtml) 
+        private static BirdDataLanguage GetBirdLanguageData(string html, string languageClassHtml, string languageId) 
         {
-            var nodeBirdLanguage = GetNodes(html, $"//li[@class= '{LANGUAGE_FR}']//a");
+            var nodeBirdLanguage = GetNodes(html, $"//li[@class= '{languageClassHtml}']//a");
             if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
             {
-                var url_FR = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
-                var birdPageHtml = GetTheHtml(url_FR);
+                var birdDataLanguage = new BirdDataLanguage();
+                var url = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
+                var birdPageHtml = GetTheHtml(url);
 
-                var birdDataLanguage = new BirdDataLanguage()
-                {
-                    Url = url_FR,
-                    Name = HttpUtility.HtmlDecode(GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b").FirstOrDefault().InnerText),
-                    LanguageId = Guid.Parse(ID_LANGUAGE_FR)
-                };
+                birdDataLanguage.Url = url;
+                var node = GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b");
+                birdDataLanguage.Name = node != null && node.Any() ? HttpUtility.HtmlEncode(node.FirstOrDefault().InnerText) : "NoName";
+                birdDataLanguage.LanguageId = Guid.Parse(languageId);
                 return birdDataLanguage;
             }
+
+            return null;
+        }
+
+        private static BirdDataLanguage GetBirdLanguageData_FR(string html, string languageClassHtml, string languageId)
+        {
+            var nodeBirdLanguage = GetNodes(html, $"//li[@class= '{languageClassHtml}']//a");
+            if (nodeBirdLanguage != null && nodeBirdLanguage.Any())
+            {
+                var birdDataLanguage = new BirdDataLanguage();
+
+                var url_FR = nodeBirdLanguage.FirstOrDefault().Attributes["href"].Value;
+                
+                var birdPageHtml = GetTheHtml(url_FR);
+
+                birdDataLanguage.Url = url_FR;
+                var node = GetNodes(birdPageHtml, "//div[@class= 'mw-parser-output']//p//b");
+                birdDataLanguage.Name = node != null && node.Any() ? HttpUtility.HtmlEncode(node.FirstOrDefault().InnerText) : "NoName";
+                birdDataLanguage.LanguageId = Guid.Parse(languageId);
+                return birdDataLanguage;
+            }
+
+            return null;
         }
 
         private static string GetTheHtml(string url)
@@ -196,37 +160,55 @@ namespace FL.Console.Robot.Species
 
         private static void InstertInDatabse(BirdSpecie birdSpecie) 
         {
-            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ComputerShop;Integrated Security=True");
-            string query = $"INSERT INTO [dbo].[BirdSpecies] ([SpeciesId],[ScienceName]) VALUES ({birdSpecie.SpecieId} ,{birdSpecie.ScienceName})";
+            SqlConnection con = new SqlConnection(@"data source=DESKTOP-JIFKME0\SQLEXPRESS; initial catalog=BirdSpecies; Integrated Security=True;");
+            string query = $"INSERT INTO [dbo].[BirdSpecies] ([SpeciesId],[ScienceName]) VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.ScienceName}')";
             SqlCommand cmd = new SqlCommand(query, con);
             try
             {
                 con.Open();
                 cmd.ExecuteNonQuery();
 
-                var query_EN = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki]) VALUES ({birdSpecie.SpecieId} ,{birdSpecie.Data_EN.LanguageId} ,{birdSpecie.Data_EN.Name} ,{birdSpecie.Data_EN.Url})";
-                cmd = new SqlCommand(query_EN, con);
-                cmd.ExecuteNonQuery();
+                if (birdSpecie.Data_EN != null)
+                {
+                    var query_EN = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki]) VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.Data_EN.LanguageId}' ,'{birdSpecie.Data_EN.Name}' , '{birdSpecie.Data_EN.Url}')";
+                    cmd = new SqlCommand(query_EN, con);
+                    cmd.ExecuteNonQuery();
+                }
 
-                var query_ES = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ({birdSpecie.SpecieId} ,{birdSpecie.Data_ES.LanguageId} ,{birdSpecie.Data_ES.Name} ,{birdSpecie.Data_ES.Url})";
-                cmd = new SqlCommand(query_ES, con);
-                cmd.ExecuteNonQuery();
+                if (birdSpecie.Data_ES != null)
+                {
+                    var query_ES = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.Data_ES.LanguageId}' ,'{birdSpecie.Data_ES.Name}' ,'{birdSpecie.Data_ES.Url}')";
+                    cmd = new SqlCommand(query_ES, con);
+                    cmd.ExecuteNonQuery();
+                }
 
-                var query_DE = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ({birdSpecie.SpecieId} ,{birdSpecie.Data_DE.LanguageId} ,{birdSpecie.Data_DE.Name} ,{birdSpecie.Data_DE.Url})";
-                cmd = new SqlCommand(query_DE, con);
-                cmd.ExecuteNonQuery();
+                if (birdSpecie.Data_DE != null)
+                {
+                    var query_DE = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.Data_DE.LanguageId}' ,'{birdSpecie.Data_DE.Name}' ,'{birdSpecie.Data_DE.Url}')";
+                    cmd = new SqlCommand(query_DE, con);
+                    cmd.ExecuteNonQuery();
+                }
 
-                var query_IT = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ({birdSpecie.SpecieId} ,{birdSpecie.Data_IT.LanguageId} ,{birdSpecie.Data_IT.Name} ,{birdSpecie.Data_IT.Url})";
-                cmd = new SqlCommand(query_IT, con);
-                cmd.ExecuteNonQuery();
+                if (birdSpecie.Data_IT != null)
+                {
+                    var query_IT = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.Data_IT.LanguageId}' ,'{birdSpecie.Data_IT.Name}' ,'{birdSpecie.Data_IT.Url}')";
+                    cmd = new SqlCommand(query_IT, con);
+                    cmd.ExecuteNonQuery();
+                }
 
-                var query_FR = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ({birdSpecie.SpecieId} ,{birdSpecie.Data_FR.LanguageId} ,{birdSpecie.Data_FR.Name} ,{birdSpecie.Data_FR.Url})";
-                cmd = new SqlCommand(query_FR, con);
-                cmd.ExecuteNonQuery();
+                if (birdSpecie.Data_FR != null)
+                {
+                    var query_FR = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.Data_FR.LanguageId}' ,'{birdSpecie.Data_FR.Name}' ,'{birdSpecie.Data_FR.Url}')";
+                    cmd = new SqlCommand(query_FR, con);
+                    cmd.ExecuteNonQuery();
+                }
 
-                var query_PT = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ({birdSpecie.SpecieId} ,{birdSpecie.Data_PT.LanguageId} ,{birdSpecie.Data_PT.Name} ,{birdSpecie.Data_PT.Url})";
-                cmd = new SqlCommand(query_PT, con);
-                cmd.ExecuteNonQuery();
+                if (birdSpecie.Data_PT != null)
+                {
+                    var query_PT = $"INSERT INTO [dbo].[BirdSpeciesLanguages] ([SpecieId] ,[LanguageId] ,[Name] ,[Url_Wiki])  VALUES ('{birdSpecie.SpecieId}' ,'{birdSpecie.Data_PT.LanguageId}' ,'{birdSpecie.Data_PT.Name}' ,'{birdSpecie.Data_PT.Url}')";
+                    cmd = new SqlCommand(query_PT, con);
+                    cmd.ExecuteNonQuery();
+                }
             }
             catch (SqlException e)
             {
