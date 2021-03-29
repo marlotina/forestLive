8f9b41d4199ac017651fd35cc13f090dac0da554
marlotina <inaki.arrieta@gmail.com>
2021-03-29 20:57:19 +0200

    remove usings

--- ForestLive/FL.Web.API.Core.UserLabels/Controllers/v1/UserLabelsController.cs
+++ ForestLive/FL.Web.API.Core.UserLabels/Controllers/v1/UserLabelsController.cs
@@ -1,8 +1,6 @@
 ﻿using FL.LogTrace.Contracts.Standard;
 using FL.Pereza.Helpers.Standard.JwtToken;
-using FL.Web.API.Core.User.Posts.Api.Models.v1.Request;
 using FL.Web.API.Core.User.Posts.Application.Exceptions;
-using FL.Web.API.Core.User.Posts.Application.Services.Contracts;
 using FL.WebAPI.Core.UserLabels.Api.Mapper.v1.Contracts;
 using FL.WebAPI.Core.UserLabels.Api.Models.v1.Request;
 using FL.WebAPI.Core.UserLabels.Application.Services.Contracts;
